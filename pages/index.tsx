11d5648c42366f5b4288caf44cd9e5ec7484f9c3
mariia.synelnyk <44849051+Mariesnlk@users.noreply.github.com>
2023-01-06 15:33:17 +0100

    read from solana nodes

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,25 +2,35 @@ import type { NextPage } from 'next'
 import { useState } from 'react'
 import styles from '../styles/Home.module.css'
 import AddressForm from '../components/AddressForm'
-import * as Web3 from '@solana/web3.js'
+import * as web3 from '@solana/web3.js'
 
 const Home: NextPage = () => {
   const [balance, setBalance] = useState(0)
   const [address, setAddress] = useState('')
+  const [executable, setIsExecutable] = useState(false)
 
   const addressSubmittedHandler = (address: string) => {
     try {
       setAddress(address)
-      const key = new Web3.PublicKey(address)
-      const connection = new Web3.Connection(Web3.clusterApiUrl('devnet'))
+      const key = new web3.PublicKey(address)
+      const connection = new web3.Connection(web3.clusterApiUrl('devnet'))
+      /// converting the balance from Lamports to SOL - the balance is returned in Lamports, not SOL.
       connection.getBalance(key).then(balance => {
-        setBalance(balance / Web3.LAMPORTS_PER_SOL)
+        setBalance(balance / web3.LAMPORTS_PER_SOL)
+      })
+
+      connection.getAccountInfo(key).then(info => {
+        setIsExecutable(info?.executable ?? false)
       })
     } catch (error) {
       setAddress('')
       setBalance(0)
       alert(error)
     }
+
+    /// This is the encoding of Solana addresses as strings.
+    // setAddress(key.toBase58())
+
   }
 
   return (
@@ -32,6 +42,7 @@ const Home: NextPage = () => {
         <AddressForm handler={addressSubmittedHandler} />
         <p>{`Address: ${address}`}</p>
         <p>{`Balance: ${balance} SOL`}</p>
+        <p>{`Is it executable?: ${executable ? 'Yep' : 'Nope'} `}</p>
       </header>
     </div>
   )
